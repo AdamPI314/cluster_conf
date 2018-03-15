68ea7a823726c0d885a3b05823a8c0703d05b60d
AdamPI <shirong.bai@colorado.edu>
2018-03-15 01:07:45 -0600

    minor

--- scripts/3_ssh_key_to_a_node.sh
+++ scripts/3_ssh_key_to_a_node.sh
@@ -1,49 +1,12 @@
 #!/bin/bash
 
 # Administrator username and password on the destination node
-NODE_NAME=$1
+ADMIN_USERNAME=$1
 NODE_IP=$2
-ADMIN_USERNAME=$3
 
 # update ssh config file to ignore unknown host
 if ! [ -d ~/.ssh ]; then
     mkdir ~/.ssh/
-fi
-
-pattern0="Host headnode"
-if grep -q "${pattern0}" ~/.ssh/config; then
-    echo $pattern0 found
-else
-    echo Host headnode >> ~/.ssh/config;
-fi
-
-pattern1="Host node"
-if grep -q "${pattern1}" ~/.ssh/config; then
-    echo $pattern1 found
-else
-    echo Host node\* >> ~/.ssh/config;
-fi
-
-pattern2="StrictHostKeyChecking no"
-if grep -q "${pattern2}" ~/.ssh/config; then
-    echo $pattern2 found
-else
-    echo StrictHostKeyChecking no >> ~/.ssh/config;
-fi
-
-pattern3="UserKnownHostsFile"
-if grep -q "${pattern3}" ~/.ssh/config; then
-    echo $pattern3 found
-else
-    echo UserKnownHostsFile=/dev/null >> ~/.ssh/config
-fi
-
-# add node ip, node name pair to /etc/hosts
-pattern4="${NODE_IP} ${NODE_NAME}"
-if grep -q "${pattern4}" /etc/hosts; then
-    echo $pattern4 found
-else
-    echo ${pattern4} >> /etc/hosts
 fi
 
 # generate a set of sshkey under ~/.ssh if there is not one yet
