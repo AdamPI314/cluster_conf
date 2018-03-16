7cc88cea3ea3dcbe271ca7f376b16baf49186fb2
AdamPI314 <shirong.bai@colorado.edu>
2018-03-16 17:39:15 -0600

    cpu usage

--- scripts/4_cpu_usage.sh
+++ scripts/4_cpu_usage.sh
@@ -1,28 +1,32 @@
-#!/bin/bash
-
-own=$(id -nu)
-
-for user in $(who | awk '{print $1}' | sort -u)
-do
-    # print other user's CPU usage in parallel but skip own one because
-    # spawning many processes will increase our CPU usage significantly
-    if [ "$user" = "$own" ]; then continue; fi
-        top -bn2 -u "$user" | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk -v user=$user, '{print user 100 - $1"%"}' &
-done
-wait
-
-# print own CPU usage after all spawned processes completed
-top -bn2 -u "$own" | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk -v user=$own, '{print user, 100 - $1"%"}'
-
 # #!/bin/bash
+
+# own=$(id -nu)
 
 # for user in $(who | awk '{print $1}' | sort -u)
 # do
 #     # print other user's CPU usage in parallel but skip own one because
 #     # spawning many processes will increase our CPU usage significantly
-#         top -bn2 -u "$user" | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk -v user=$user, '{print user, 100 - $1"%"}' &
+#     if [ "$user" = "$own" ]; then continue; fi
+#         top -bn2 -u "$user" | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk -v user=$user, '{print user 100 - $1"%"}' &
 # done
 # wait
 
-# # print own CPU usage after all spawned processes completed
-# top -bn2 | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "OVERALL,", 100 - $1"%"}'
+# print own CPU usage after all spawned processes completed
+top -bn2 -u "$own" | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk -v user=$own, '{print user, 100 - $1"%"}'
+
+#!/bin/bash
+
+for user in $(who | awk '{print $1}' | sort -u)
+do
+    # print other user's CPU usage in parallel but skip own one because
+    # spawning many processes will increase our CPU usage significantly
+        (ncpu=$(lscpu | grep "^CPU(s)" | awk '{print $2}'); \
+                ratio=$(echo "1.0/$ncpu" | bc -l); \
+                nline1=$(top -bn1 -u "$user" | wc -l); \
+                nline2=$(top -bn2 -u "$user" | wc -l); \
+                top -bn2 -u "$user" | tail -$(($nline2-$nline1)) | grep "$user" | awk -v user="$user" -v ratio=$ratio, '{ sum += $9; } END { print user, sum*ratio"%"; }') &
+done
+wait
+
+# print own CPU usage after all spawned processes completed
+top -bn2 | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "OVERALL,", 100 - $1"%"}'
