314efa6b0e77db53b259fcc5aa53c5c6e28026ed
AdamPI314 <shirong.bai@colorado.edu>
2018-04-09 16:41:36 -0600

    minor

--- scripts/4_cpu_usage.sh
+++ scripts/4_cpu_usage.sh
@@ -18,14 +18,14 @@
 
 # for user in $(who | awk '{print $1}' | sort -u)
 for user in $(ps aux | awk '{ print $1 }' | sed '1 d' | sort -u | uniq); do
-    if [[ $(getent group SkodjeLab | grep &>/dev/null "\b${user}\b") || $(getent group sudo | grep &>/dev/null "\b${user}\b") ]]; then
-            # print other user's CPU usage in parallel but skip own one because
-            # spawning many processes will increase our CPU usage significantly
-            (ncpu=$(lscpu | grep "^CPU(s)" | awk '{print $2}'); \
-                    ratio=$(echo "1.0/$ncpu" | bc -l); \
-                    nline1=$(top -bn1 -u "$user" | wc -l); \
-                    nline2=$(top -bn2 -u "$user" | wc -l); \
-                    top -bn2 -u "$user" | tail -$(($nline2-$nline1)) | grep "$user" | awk -v user="$user" -v ratio=$ratio, '{ sum += $9; } END { print user, sum*ratio"%"; }') &
+    if getent group SkodjeLab | grep &>/dev/null "\b${user}\b"; then
+        # print other user's CPU usage in parallel but skip own one because
+        # spawning many processes will increase our CPU usage significantly
+        (ncpu=$(lscpu | grep "^CPU(s)" | awk '{print $2}'); \
+                ratio=$(echo "1.0/$ncpu" | bc -l); \
+                nline1=$(top -bn1 -u "$user" | wc -l); \
+                nline2=$(top -bn2 -u "$user" | wc -l); \
+                top -bn2 -u "$user" | tail -$(($nline2-$nline1)) | grep "$user" | awk -v user="$user" -v ratio=$ratio, '{ sum += $9; } END { print user, sum*ratio"%"; }') &
     fi
 done
 wait
